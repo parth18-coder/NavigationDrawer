1fa0368080109078d1b39d4c0450a2faaafcb976
Parth <banparth57@gmail.com>
2023-03-31 17:13:42 +0530

    How to implement the navigation drawer

---
+++ app/src/main/java/com/example/navigationdrawer/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.navigationdrawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var toggle:ActionBarDrawerToggle
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // now we have to add the action bar to have that hamburger button to open navigation drawer by only clicking it.
+        // if we don't add the action bar then still we can open navigation drawer by just swipe left the screen.
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+
+        toggle= ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        // when navigation drawer is open than toggle button get converted into back button
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
